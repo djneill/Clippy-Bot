1304865855aea49ded438540a5672589085ed07c
DJ Neill <dj.neill856@gmail.com>
2023-10-07 23:02:34 -0500

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,113 +1,111 @@
-import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-import "./App.css";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator, Avatar } from '@chatscope/chat-ui-kit-react';
+import clippyAvatar from './assets/clippy.png'
 
-// const API_KEY = import.meta.env.API_KEY
-const API_TOKEN = import.meta.env.VITE_API_TOKEN
-const API_URL = import.meta.env.VITE_API_URL
+const API_KEY = import.meta.env.VITE_API_KEY
+// "Explain things like you would to a 10 year old learning how to code."
+const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
+  "role": "system", "content": "Explain things like you're Clippy, the Microsoft Office Assistant."
+}
+
+const chatGPTAvatar = ( // Create an avatar for ChatGPT
+  <Avatar src={clippyAvatar} name={"Clippy"} size="md" />
+)
 
 function App() {
   const [messages, setMessages] = useState([
     {
-      message: "Hello",
-      sender: "Hugging Face",
-    },
+      message: "Hello, I'm Clippy! How can I assist you today?",
+      sentTime: "just now",
+      sender: "ChatGPT",
+      avatar: chatGPTAvatar,
+    }
   ]);
   const [isTyping, setIsTyping] = useState(false);
 
   const handleSend = async (message) => {
     const newMessage = {
       message,
-      sender: "user",
-      direction: "outgoing",
-    }
+      direction: 'outgoing',
+      sender: "user"
+    };
 
-    const newMessages = [...messages, newMessage]; //all the old messages plus the new one
+    const newMessages = [...messages, newMessage];
 
-    //update our messages state
     setMessages(newMessages);
 
-    // set typing indicatior
+    // Initial system message to determine ChatGPT functionality
+    // How it responds, how it talks, etc.
     setIsTyping(true);
-    // process message to chaGPT (send it over and see the response)
-    await processMessageToHuggingFace(newMessages)
+    await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToHuggingFace(chatMessages) {
-    // chatMessages { sender: 'user' or 'hugginface', message: 'message content' }
-    // apiMessages { role: 'user' or 'assistant', content: 'message content' }
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for chatGPT API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
 
     let apiMessages = chatMessages.map((messageObject) => {
-      let role = ''
-      if (messageObject.sender === 'HugginFace') {
-        role = 'assistant'
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
       } else {
-        role = 'user'
+        role = "user";
       }
       return { role: role, content: messageObject.message }
-    })
-
-    // role: 'user -> a message from the user, 'assistant' -> a message from the assistant
-    // system -> generally one initial message defining how we want Hugging Face to behave
-
-    const systemMessage = {
-      role: 'system',
-      content: 'Speak like you are Microsoft Clippy'
-    }
+    });
 
 
-    let apiRequestBody = {
-      'model': 'google/flan-t5-large',
-      'messages': [
-        systemMessage,
-        ...apiMessages
+    // Get the request body set up with the model we plan to use
+    // and the messages which we formatted above. We add a system message in the front to'
+    // determine how we want chatGPT to act. 
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  // The system message DEFINES the logic of our chatGPT
+        ...apiMessages // The messages from our chat with ChatGPT
       ]
     }
 
-    async function query(data) {
-
-      console.log("Request URL:", `${API_URL}`);
-      const response = await fetch(
-        `${API_URL}`,
-        {
-          headers: { Authorization: "Bearer " + API_TOKEN },
-          method: "POST",
-          body: JSON.stringify(data),
-        }
-      );
-      const result = await response.json();
-      return result;
-    }
-
-    query({ apiRequestBody }).then((data) => {
-      console.log(JSON.stringify(data));
-    }).then((data) => {
-      return data.json()
-    }).then((data) => {
-      console.log(data)
-      console.log(data.choices[0].message.content)
-      setMessages(
-        [...chatMessages, {
+    await fetch("https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      }).then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        setMessages([...chatMessages, {
           message: data.choices[0].message.content,
-          sender: "HuggingFace",
-        }]
-      )
-      setIsTyping(false)
-    })
+          sender: "ChatGPT",
+          avatar: chatGPTAvatar,
+        }]);
+        setIsTyping(false);
+      });
   }
 
   return (
     <div className="App">
-      <div style={{ position: "relative", height: "700px", width: "700px" }}>
+      <div style={{ position: "relative", height: "700px", width: "700px", }}>
         <MainContainer>
           <ChatContainer>
             <MessageList
-              scrollBehavior='smooth'
-              typingIndicatior={isTyping ? <TypingIndicator content="HuggingFace is typing..." /> : null}>
+              scrollBehavior="smooth"
+              typingIndicator={isTyping ? <TypingIndicator content="Clippy is typing" /> : null}
+            >
               {messages.map((message, i) => {
-                return <Message key={i} model={message} />;
+                console.log(message)
+                return (
+                  <Message key={i} model={message}>
+                    {message.sender === "ChatGPT" && message.avatar}
+                  </Message>
+                )
               })}
             </MessageList>
             <MessageInput placeholder="Type message here" onSend={handleSend} />
@@ -115,7 +113,7 @@ function App() {
         </MainContainer>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
